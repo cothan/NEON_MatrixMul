da078c5c2517bfe77e3d3c3a14af4ce9f34422c6
cothan <cothannguyen@gmail.com>
2021-02-17 16:07:41 -0500

    todo: test

--- matmul_armv7.c
+++ matmul_armv7.c
@@ -1,29 +1,27 @@
 #include <arm_neon.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef uint16_t u16;
 
 // Correct with N = 4, 12, 20
 // gcc matmul.c -o matmul -O3  -g3 -mcpu=cortex-a9 -mfloat-abi=hard -mfpu=neon
 
-
-#define N 8
+#define N 252
 #define MAXRAND 8
+#ifndef DEBUG
+#define DEBUG 0
+#endif
 
 #if (N % 8) == 0
 
 #define BLOCKSIZE 8
-#define BLOCKSIZE_HALF 4
 typedef uint16x8_t vec;
 typedef uint16x8x4_t vecval;
-typedef uint16x4x4_t vecval2;
 #define vload(c, ptr) c = vld1q_u16(ptr);
-#define vload_half(c, ptr) c = vld1_u16(ptr);
-#define vloadx4(c, ptr) c = vld1q_u16_x4(ptr);
 #define vstore(ptr, c) vst1q_u16(ptr, c);
-#define vstorex4(ptr, c) vst1q_u16_x4(ptr, c);
 #define vmla(c, a, b) c = vmlaq_u16(c, a, b);
-#define vmlalane(c, a, b, n) c = vmlaq_lane_u16(c, a, b, n);
+#define vmlalane(c, a, b, n) c = vmlaq_laneq_u16(c, a, b, n);
 #define vdup(c, n) c = vdupq_n_u16(n);
 
 #elif (N % 4) == 0
@@ -32,9 +30,7 @@ typedef uint16x4x4_t vecval2;
 typedef uint16x4_t vec;
 typedef uint16x4x4_t vecval;
 #define vload(c, ptr) c = vld1_u16(ptr);
-#define vloadx4(c, ptr) c = vld1_u16_x4(ptr);
 #define vstore(ptr, c) vst1_u16(ptr, c);
-#define vstorex4(ptr, c) vst1_u16_x4(ptr, c);
 #define vmla(c, a, b) c = vmla_u16(c, a, b);
 #define vmlalane(c, a, b, n) c = vmla_lane_u16(c, a, b, n);
 #define vdup(c, n) c = vdup_n_u16(n);
@@ -46,52 +42,45 @@ typedef uint16x4x4_t vecval;
 
 void print_array(u16 *a)
 {
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			printf("%4d, ", a[i * N + j]);
-		}
-		printf("\n");
-	}
-	printf("=======\n");
-}
-
-void print_vector_half(vecval2 a)
-{
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < BLOCKSIZE_HALF; j++)
-		{
-			printf("%4d, ", a.val[i][j]);
-		}
-		printf("\n");
-	}
-	printf("--\n");
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%4d, ", a[i * N + j]);
+        }
+        printf("\n");
+    }
+    printf("=======\n");
 }
 
 void print_vector(vecval a)
 {
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < BLOCKSIZE; j++)
-		{
-			printf("%4d, ", a.val[i][j]);
-		}
-		printf("\n");
-	}
-	printf("--\n");
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < BLOCKSIZE; j++)
+        {
+            printf("%4d, ", a.val[i][j]);
+        }
+        printf("\n");
+    }
+    printf("--\n");
 }
 
 void classicMatMult(u16 A[], u16 B[], u16 C[])
 {
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			for (int k = 0; k < N; k++)
-			{
-				C[i * N + j] += A[i * N + k] * B[k * N + j];
-				// printf("%d * %d\n", i * N + k , k * N + j);
-			}
+    for (int i = 0; i < N; i++)
+    {
+        for (int k = 0; k < N; k++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                C[i * N + j] += A[i * N + k] * B[k * N + j];
+                // printf("%2d * %2d, ", A[i * N + k], B[k * N + j]);
+            }
+            // printf("\n");
+        }
+        // printf("\n");
+    }
 }
 
 #if (N % 8) == 0
@@ -364,70 +353,77 @@ void neonMatMul_base(u16 A[], u16 B[], u16 C[])
 #else
 #error "Matrix must be multiple of {4, 8}"
 #endif
-
 void neoMatMul(u16 A[], u16 B[], u16 C[])
 {
-	for (int i = 0; i < N; i += BLOCKSIZE)
-		for (int j = 0; j < N; j += BLOCKSIZE)
-		{
-			for (int k = 0; k < N; k += BLOCKSIZE)
-			{
-				neonMatMul_base(&A[i + N*k], &B[N*j + k], &C[N*j + i]);
-			}
-		}
+    for (int i = 0; i < N; i += BLOCKSIZE)
+    {
+        for (int k = 0; k < N; k += BLOCKSIZE)
+        {
+            for (int j = 0; j < N; j += BLOCKSIZE)
+            {
+                neonMatMul_base(&A[i * N + k], &B[k * N + j], &C[N * i + j]);
+            }
+        }
+    }
 }
 
 int checkCorrect(u16 A[], u16 B[])
 {
-	int result = 0;
-	for (int i = 0; i < N * N; i++)
-	{
-		if (A[i] != B[i])
-		{
-			return 1;
-		}
-	}
-	return 0;
+    for (int i = 0; i < N * N; i++)
+    {
+        if (A[i] != B[i])
+        {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 #define TESTS 1
 
 int main()
 {
-	u16 A[N * N], B[N * N], C[N * N] = {0}, D[N * N] = {0};
-	u16 M[N * N];
-	for (int i = 0; i < N * N; i++)
-	{
-		A[i] = (u16)rand() % MAXRAND;
-		// A[i] = i ;//% MAXRAND;
-		B[i] = (u16)rand() % MAXRAND;
-		// B[i] = i ;//% MAXRAND;
-		// C[i] = i ;
-		// D[i] = i;
-	}
+    srand(time(0));
+    u16 A[N * N], B[N * N], C[N * N] = {0}, D[N * N] = {0};
+    for (int i = 0; i < N * N; i++)
+    {
+        A[i] = rand(); //% MAXRAND;
+        B[i] = rand(); //% MAXRAND;
+        // A[i] = (i + 4);
+        // B[i] = i;
+        // C[i] = i ;
+        // D[i] = i;
+    }
 
+#if DEBUG
+    printf("A======\n");
+    print_array(A);
+    printf("B======\n");
+    print_array(B);
+#endif
 
-	for (int i = 0; i < TESTS; i++)
-	{
-		classicMatMult(A, B, C);
-	}
+    for (int i = 0; i < TESTS; i++)
+    {
+        classicMatMult(A, B, C);
+    }
 
+    for (int i = 0; i < TESTS; i++)
+    {
+        neoMatMul(A, B, D);
+    }
 
-	for (int i = 0; i < TESTS; i++)
-	{
-		neoMatMul(A, B, D);
-	}
+#if DEBUG
+    printf("C======\n");
+    print_array(C);
+    printf("D======\n");
+    print_array(D);
+#endif
 
-	print_array(A);
-	print_array(B);
-	print_array(C);
-	print_array(D);
-
-	if (checkCorrect(C, D))
-	{
-		printf("Error\n");
-		return 1;
-	}
-	printf("OK\n");
-	return 0;
+    if (checkCorrect(C, D))
+    {
+        printf("Error\n");
+        return 1;
+    }
+    printf("OK\n");
+    return 0;
 }
