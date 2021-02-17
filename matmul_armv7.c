54a6f41837f1f3ec17ad3633c5e80d739db0426e
cothan <cothannguyen@gmail.com>
2021-02-17 17:14:36 -0500

    finish

--- matmul_armv7.c
+++ matmul_armv7.c
@@ -4,13 +4,15 @@
 
 typedef uint16_t u16;
 
-// Correct with N = 4, 12, 20
-// gcc matmul.c -o matmul -O3  -g3 -mcpu=cortex-a9 -mfloat-abi=hard -mfpu=neon
-
+// Correct with N = 4, 8, 12, 16, 20, 24, 28, 32, 36
+// gcc matmul.c -o matmul -O3  -g3 -mcpu=cortex-a9 -mfloat-abi=hard -mfpu=neon -DN=32 -DCOMPRESS=0
 #define N 8
 #define MAXRAND 8
 #ifndef DEBUG
-#define DEBUG 1
+#define DEBUG 0
+#endif
+#ifndef COMPRESS
+#define COMPRESS 0
 #endif
 
 #if (N % 8) == 0
@@ -88,7 +90,7 @@ void classicMatMult(u16 A[], u16 B[], u16 C[])
 #if (N % 8) == 0
 void print_vector_half(vecval2 a)
 {
-    for (int i = 0; i < 4; i+=2)
+    for (int i = 0; i < 4; i += 2)
     {
         for (int j = 0; j < 4; j++)
         {
@@ -96,7 +98,7 @@ void print_vector_half(vecval2 a)
         }
         for (int j = 0; j < 4; j++)
         {
-            printf("%4d, ", a.val[i+1][j]);
+            printf("%4d, ", a.val[i + 1][j]);
         }
         printf("\n");
     }
@@ -105,55 +107,80 @@ void print_vector_half(vecval2 a)
 
 void neonMatMul_base(u16 A[], u16 B[], u16 C[])
 {
-	// Total registers:  24
-	vecval vb1, vb2, vc1, vc2;
-    vecval2 va1, va2, va3, va4;
+    // Total registers:  24
+    vecval vb1, vb2, vc1, vc2;
 
-	vload_half(va1.val[0], &A[0 * N]);
-	vload_half(va1.val[1], &A[0 * N + 4]);
-	vload_half(va1.val[2], &A[1 * N]);
-	vload_half(va1.val[3], &A[1 * N + 4]);
+#if COMPRESS
+    vecval2 va[4];
 
-	vload_half(va2.val[0], &A[2 * N]);
-	vload_half(va2.val[1], &A[2 * N + 4]);
-	vload_half(va2.val[2], &A[3 * N]);
-	vload_half(va2.val[3], &A[3 * N + 4]);
+    vload_half(va[0].val[0], &A[0 * N]);
+    vload_half(va[0].val[1], &A[0 * N + 4]);
+    vload_half(va[0].val[2], &A[1 * N]);
+    vload_half(va[0].val[3], &A[1 * N + 4]);
 
-    vload_half(va3.val[0], &A[4 * N]);
-	vload_half(va3.val[1], &A[4 * N + 4]);
-	vload_half(va3.val[2], &A[5 * N]);
-	vload_half(va3.val[3], &A[5 * N + 4]);
+    vload_half(va[1].val[0], &A[2 * N]);
+    vload_half(va[1].val[1], &A[2 * N + 4]);
+    vload_half(va[1].val[2], &A[3 * N]);
+    vload_half(va[1].val[3], &A[3 * N + 4]);
 
-	vload_half(va4.val[0], &A[6 * N]);
-	vload_half(va4.val[1], &A[6 * N + 4]);
-	vload_half(va4.val[2], &A[7 * N]);
-	vload_half(va4.val[3], &A[7 * N + 4]);
-    
-	vload(vb1.val[0], &B[0 * N]);
-	vload(vb1.val[1], &B[1 * N]);
-	vload(vb1.val[2], &B[2 * N]);
-	vload(vb1.val[3], &B[3 * N]);
+    vload_half(va[2].val[0], &A[4 * N]);
+    vload_half(va[2].val[1], &A[4 * N + 4]);
+    vload_half(va[2].val[2], &A[5 * N]);
+    vload_half(va[2].val[3], &A[5 * N + 4]);
 
-	vload(vb2.val[0], &B[4 * N]);
-	vload(vb2.val[1], &B[5 * N]);
-	vload(vb2.val[2], &B[6 * N]);
-	vload(vb2.val[3], &B[7 * N]);
+    vload_half(va[3].val[0], &A[6 * N]);
+    vload_half(va[3].val[1], &A[6 * N + 4]);
+    vload_half(va[3].val[2], &A[7 * N]);
+    vload_half(va[3].val[3], &A[7 * N + 4]);
+#else
+    vecval2 va0, va1, va2, va3;
 
-	vload(vc1.val[0], &C[0 * N]);
-	vload(vc1.val[1], &C[1 * N]);
-	vload(vc1.val[2], &C[2 * N]);
-	vload(vc1.val[3], &C[3 * N]);
+    vload_half(va0.val[0], &A[0 * N]);
+    vload_half(va0.val[1], &A[0 * N + 4]);
+    vload_half(va0.val[2], &A[1 * N]);
+    vload_half(va0.val[3], &A[1 * N + 4]);
 
-	vload(vc2.val[0], &C[4 * N]);
-	vload(vc2.val[1], &C[5 * N]);
-	vload(vc2.val[2], &C[6 * N]);
-	vload(vc2.val[3], &C[7 * N]);
+    vload_half(va1.val[0], &A[2 * N]);
+    vload_half(va1.val[1], &A[2 * N + 4]);
+    vload_half(va1.val[2], &A[3 * N]);
+    vload_half(va1.val[3], &A[3 * N + 4]);
+
+    vload_half(va2.val[0], &A[4 * N]);
+    vload_half(va2.val[1], &A[4 * N + 4]);
+    vload_half(va2.val[2], &A[5 * N]);
+    vload_half(va2.val[3], &A[5 * N + 4]);
+
+    vload_half(va3.val[0], &A[6 * N]);
+    vload_half(va3.val[1], &A[6 * N + 4]);
+    vload_half(va3.val[2], &A[7 * N]);
+    vload_half(va3.val[3], &A[7 * N + 4]);
+#endif
+
+    vload(vb1.val[0], &B[0 * N]);
+    vload(vb1.val[1], &B[1 * N]);
+    vload(vb1.val[2], &B[2 * N]);
+    vload(vb1.val[3], &B[3 * N]);
+
+    vload(vb2.val[0], &B[4 * N]);
+    vload(vb2.val[1], &B[5 * N]);
+    vload(vb2.val[2], &B[6 * N]);
+    vload(vb2.val[3], &B[7 * N]);
+
+    vload(vc1.val[0], &C[0 * N]);
+    vload(vc1.val[1], &C[1 * N]);
+    vload(vc1.val[2], &C[2 * N]);
+    vload(vc1.val[3], &C[3 * N]);
+
+    vload(vc2.val[0], &C[4 * N]);
+    vload(vc2.val[1], &C[5 * N]);
+    vload(vc2.val[2], &C[6 * N]);
+    vload(vc2.val[3], &C[7 * N]);
 
 #if DEBUG
-    print_vector_half(va1);
-    print_vector_half(va2);
-    print_vector_half(va3);
-    print_vector_half(va4);
+    print_vector_half(va[0]);
+    print_vector_half(va[1]);
+    print_vector_half(va[2]);
+    print_vector_half(va[3]);
 
     print_vector(vb1);
     print_vector(vb2);
@@ -161,141 +188,188 @@ void neonMatMul_base(u16 A[], u16 B[], u16 C[])
     print_vector(vc2);
 #endif
 
+#if COMPRESS
+    for (int i = 0; i < 4; i += 2)
+    {
+        vmlalane(vc1.val[i], vb1.val[0], va[i / 2].val[0], 0);
+        vmlalane(vc1.val[i], vb1.val[1], va[i / 2].val[0], 1);
+        vmlalane(vc1.val[i], vb1.val[2], va[i / 2].val[0], 2);
+        vmlalane(vc1.val[i], vb1.val[3], va[i / 2].val[0], 3);
 
-    vmlalane(vc1.val[0], vb1.val[0],  va1.val[0], 0);
-    vmlalane(vc1.val[0], vb1.val[1],  va1.val[0], 1);
-    vmlalane(vc1.val[0], vb1.val[2],  va1.val[0], 2);
-    vmlalane(vc1.val[0], vb1.val[3],  va1.val[0], 3);
+        vmlalane(vc1.val[i], vb2.val[0], va[i / 2].val[1], 0);
+        vmlalane(vc1.val[i], vb2.val[1], va[i / 2].val[1], 1);
+        vmlalane(vc1.val[i], vb2.val[2], va[i / 2].val[1], 2);
+        vmlalane(vc1.val[i], vb2.val[3], va[i / 2].val[1], 3);
 
-    vmlalane(vc1.val[0], vb2.val[0],  va1.val[1], 0);
-    vmlalane(vc1.val[0], vb2.val[1],  va1.val[1], 1);
-    vmlalane(vc1.val[0], vb2.val[2],  va1.val[1], 2);
-    vmlalane(vc1.val[0], vb2.val[3],  va1.val[1], 3);
+        vmlalane(vc1.val[i + 1], vb1.val[0], va[i / 2].val[2], 0);
+        vmlalane(vc1.val[i + 1], vb1.val[1], va[i / 2].val[2], 1);
+        vmlalane(vc1.val[i + 1], vb1.val[2], va[i / 2].val[2], 2);
+        vmlalane(vc1.val[i + 1], vb1.val[3], va[i / 2].val[2], 3);
 
-    vmlalane(vc1.val[1], vb1.val[0],  va1.val[2], 0);
-    vmlalane(vc1.val[1], vb1.val[1],  va1.val[2], 1);
-    vmlalane(vc1.val[1], vb1.val[2],  va1.val[2], 2);
-    vmlalane(vc1.val[1], vb1.val[3],  va1.val[2], 3);
+        vmlalane(vc1.val[i + 1], vb2.val[0], va[i / 2].val[3], 0);
+        vmlalane(vc1.val[i + 1], vb2.val[1], va[i / 2].val[3], 1);
+        vmlalane(vc1.val[i + 1], vb2.val[2], va[i / 2].val[3], 2);
+        vmlalane(vc1.val[i + 1], vb2.val[3], va[i / 2].val[3], 3);
+    }
+    for (int i = 0; i < 4; i += 2)
+    {
+        vmlalane(vc2.val[i], vb1.val[0], va[2 + i / 2].val[0], 0);
+        vmlalane(vc2.val[i], vb1.val[1], va[2 + i / 2].val[0], 1);
+        vmlalane(vc2.val[i], vb1.val[2], va[2 + i / 2].val[0], 2);
+        vmlalane(vc2.val[i], vb1.val[3], va[2 + i / 2].val[0], 3);
 
-    vmlalane(vc1.val[1], vb2.val[0],  va1.val[3], 0);
-    vmlalane(vc1.val[1], vb2.val[1],  va1.val[3], 1);
-    vmlalane(vc1.val[1], vb2.val[2],  va1.val[3], 2);
-    vmlalane(vc1.val[1], vb2.val[3],  va1.val[3], 3);
+        vmlalane(vc2.val[i], vb2.val[0], va[2 + i / 2].val[1], 0);
+        vmlalane(vc2.val[i], vb2.val[1], va[2 + i / 2].val[1], 1);
+        vmlalane(vc2.val[i], vb2.val[2], va[2 + i / 2].val[1], 2);
+        vmlalane(vc2.val[i], vb2.val[3], va[2 + i / 2].val[1], 3);
 
-    vmlalane(vc1.val[2], vb1.val[0],  va2.val[0], 0);
-    vmlalane(vc1.val[2], vb1.val[1],  va2.val[0], 1);
-    vmlalane(vc1.val[2], vb1.val[2],  va2.val[0], 2);
-    vmlalane(vc1.val[2], vb1.val[3],  va2.val[0], 3);
+        vmlalane(vc2.val[i + 1], vb1.val[0], va[2 + i / 2].val[2], 0);
+        vmlalane(vc2.val[i + 1], vb1.val[1], va[2 + i / 2].val[2], 1);
+        vmlalane(vc2.val[i + 1], vb1.val[2], va[2 + i / 2].val[2], 2);
+        vmlalane(vc2.val[i + 1], vb1.val[3], va[2 + i / 2].val[2], 3);
 
-    vmlalane(vc1.val[2], vb2.val[0],  va2.val[1], 0);
-    vmlalane(vc1.val[2], vb2.val[1],  va2.val[1], 1);
-    vmlalane(vc1.val[2], vb2.val[2],  va2.val[1], 2);
-    vmlalane(vc1.val[2], vb2.val[3],  va2.val[1], 3);
+        vmlalane(vc2.val[i + 1], vb2.val[0], va[2 + i / 2].val[3], 0);
+        vmlalane(vc2.val[i + 1], vb2.val[1], va[2 + i / 2].val[3], 1);
+        vmlalane(vc2.val[i + 1], vb2.val[2], va[2 + i / 2].val[3], 2);
+        vmlalane(vc2.val[i + 1], vb2.val[3], va[2 + i / 2].val[3], 3);
+    }
+#else
 
-    vmlalane(vc1.val[3], vb1.val[0],  va2.val[2], 0);
-    vmlalane(vc1.val[3], vb1.val[1],  va2.val[2], 1);
-    vmlalane(vc1.val[3], vb1.val[2],  va2.val[2], 2);
-    vmlalane(vc1.val[3], vb1.val[3],  va2.val[2], 3);
+    vmlalane(vc1.val[0], vb1.val[0], va0.val[0], 0);
+    vmlalane(vc1.val[0], vb1.val[1], va0.val[0], 1);
+    vmlalane(vc1.val[0], vb1.val[2], va0.val[0], 2);
+    vmlalane(vc1.val[0], vb1.val[3], va0.val[0], 3);
 
-    vmlalane(vc1.val[3], vb2.val[0],  va2.val[3], 0);
-    vmlalane(vc1.val[3], vb2.val[1],  va2.val[3], 1);
-    vmlalane(vc1.val[3], vb2.val[2],  va2.val[3], 2);
-    vmlalane(vc1.val[3], vb2.val[3],  va2.val[3], 3);
+    vmlalane(vc1.val[0], vb2.val[0], va0.val[1], 0);
+    vmlalane(vc1.val[0], vb2.val[1], va0.val[1], 1);
+    vmlalane(vc1.val[0], vb2.val[2], va0.val[1], 2);
+    vmlalane(vc1.val[0], vb2.val[3], va0.val[1], 3);
 
-    vmlalane(vc2.val[0], vb1.val[0],  va3.val[0], 0);
-    vmlalane(vc2.val[0], vb1.val[1],  va3.val[0], 1);
-    vmlalane(vc2.val[0], vb1.val[2],  va3.val[0], 2);
-    vmlalane(vc2.val[0], vb1.val[3],  va3.val[0], 3);
+    vmlalane(vc1.val[1], vb1.val[0], va0.val[2], 0);
+    vmlalane(vc1.val[1], vb1.val[1], va0.val[2], 1);
+    vmlalane(vc1.val[1], vb1.val[2], va0.val[2], 2);
+    vmlalane(vc1.val[1], vb1.val[3], va0.val[2], 3);
 
-    vmlalane(vc2.val[0], vb2.val[0],  va3.val[1], 0);
-    vmlalane(vc2.val[0], vb2.val[1],  va3.val[1], 1);
-    vmlalane(vc2.val[0], vb2.val[2],  va3.val[1], 2);
-    vmlalane(vc2.val[0], vb2.val[3],  va3.val[1], 3);
+    vmlalane(vc1.val[1], vb2.val[0], va0.val[3], 0);
+    vmlalane(vc1.val[1], vb2.val[1], va0.val[3], 1);
+    vmlalane(vc1.val[1], vb2.val[2], va0.val[3], 2);
+    vmlalane(vc1.val[1], vb2.val[3], va0.val[3], 3);
 
-    vmlalane(vc2.val[1], vb1.val[0],  va3.val[2], 0);
-    vmlalane(vc2.val[1], vb1.val[1],  va3.val[2], 1);
-    vmlalane(vc2.val[1], vb1.val[2],  va3.val[2], 2);
-    vmlalane(vc2.val[1], vb1.val[3],  va3.val[2], 3);
+    vmlalane(vc1.val[2], vb1.val[0], va1.val[0], 0);
+    vmlalane(vc1.val[2], vb1.val[1], va1.val[0], 1);
+    vmlalane(vc1.val[2], vb1.val[2], va1.val[0], 2);
+    vmlalane(vc1.val[2], vb1.val[3], va1.val[0], 3);
 
-    vmlalane(vc2.val[1], vb2.val[0],  va3.val[3], 0);
-    vmlalane(vc2.val[1], vb2.val[1],  va3.val[3], 1);
-    vmlalane(vc2.val[1], vb2.val[2],  va3.val[3], 2);
-    vmlalane(vc2.val[1], vb2.val[3],  va3.val[3], 3);
+    vmlalane(vc1.val[2], vb2.val[0], va1.val[1], 0);
+    vmlalane(vc1.val[2], vb2.val[1], va1.val[1], 1);
+    vmlalane(vc1.val[2], vb2.val[2], va1.val[1], 2);
+    vmlalane(vc1.val[2], vb2.val[3], va1.val[1], 3);
 
-    vmlalane(vc2.val[2], vb1.val[0],  va4.val[0], 0);
-    vmlalane(vc2.val[2], vb1.val[1],  va4.val[0], 1);
-    vmlalane(vc2.val[2], vb1.val[2],  va4.val[0], 2);
-    vmlalane(vc2.val[2], vb1.val[3],  va4.val[0], 3);
+    vmlalane(vc1.val[3], vb1.val[0], va1.val[2], 0);
+    vmlalane(vc1.val[3], vb1.val[1], va1.val[2], 1);
+    vmlalane(vc1.val[3], vb1.val[2], va1.val[2], 2);
+    vmlalane(vc1.val[3], vb1.val[3], va1.val[2], 3);
 
-    vmlalane(vc2.val[2], vb2.val[0],  va4.val[1], 0);
-    vmlalane(vc2.val[2], vb2.val[1],  va4.val[1], 1);
-    vmlalane(vc2.val[2], vb2.val[2],  va4.val[1], 2);
-    vmlalane(vc2.val[2], vb2.val[3],  va4.val[1], 3);
+    vmlalane(vc1.val[3], vb2.val[0], va1.val[3], 0);
+    vmlalane(vc1.val[3], vb2.val[1], va1.val[3], 1);
+    vmlalane(vc1.val[3], vb2.val[2], va1.val[3], 2);
+    vmlalane(vc1.val[3], vb2.val[3], va1.val[3], 3);
 
-    vmlalane(vc2.val[3], vb1.val[0],  va4.val[2], 0);
-    vmlalane(vc2.val[3], vb1.val[1],  va4.val[2], 1);
-    vmlalane(vc2.val[3], vb1.val[2],  va4.val[2], 2);
-    vmlalane(vc2.val[3], vb1.val[3],  va4.val[2], 3);
+    vmlalane(vc2.val[0], vb1.val[0], va2.val[0], 0);
+    vmlalane(vc2.val[0], vb1.val[1], va2.val[0], 1);
+    vmlalane(vc2.val[0], vb1.val[2], va2.val[0], 2);
+    vmlalane(vc2.val[0], vb1.val[3], va2.val[0], 3);
 
-    vmlalane(vc2.val[3], vb2.val[0],  va4.val[3], 0);
-    vmlalane(vc2.val[3], vb2.val[1],  va4.val[3], 1);
-    vmlalane(vc2.val[3], vb2.val[2],  va4.val[3], 2);
-    vmlalane(vc2.val[3], vb2.val[3],  va4.val[3], 3);
+    vmlalane(vc2.val[0], vb2.val[0], va2.val[1], 0);
+    vmlalane(vc2.val[0], vb2.val[1], va2.val[1], 1);
+    vmlalane(vc2.val[0], vb2.val[2], va2.val[1], 2);
+    vmlalane(vc2.val[0], vb2.val[3], va2.val[1], 3);
 
+    vmlalane(vc2.val[1], vb1.val[0], va2.val[2], 0);
+    vmlalane(vc2.val[1], vb1.val[1], va2.val[2], 1);
+    vmlalane(vc2.val[1], vb1.val[2], va2.val[2], 2);
+    vmlalane(vc2.val[1], vb1.val[3], va2.val[2], 3);
 
-#if DEBUG 
+    vmlalane(vc2.val[1], vb2.val[0], va2.val[3], 0);
+    vmlalane(vc2.val[1], vb2.val[1], va2.val[3], 1);
+    vmlalane(vc2.val[1], vb2.val[2], va2.val[3], 2);
+    vmlalane(vc2.val[1], vb2.val[3], va2.val[3], 3);
+
+    vmlalane(vc2.val[2], vb1.val[0], va3.val[0], 0);
+    vmlalane(vc2.val[2], vb1.val[1], va3.val[0], 1);
+    vmlalane(vc2.val[2], vb1.val[2], va3.val[0], 2);
+    vmlalane(vc2.val[2], vb1.val[3], va3.val[0], 3);
+
+    vmlalane(vc2.val[2], vb2.val[0], va3.val[1], 0);
+    vmlalane(vc2.val[2], vb2.val[1], va3.val[1], 1);
+    vmlalane(vc2.val[2], vb2.val[2], va3.val[1], 2);
+    vmlalane(vc2.val[2], vb2.val[3], va3.val[1], 3);
+
+    vmlalane(vc2.val[3], vb1.val[0], va3.val[2], 0);
+    vmlalane(vc2.val[3], vb1.val[1], va3.val[2], 1);
+    vmlalane(vc2.val[3], vb1.val[2], va3.val[2], 2);
+    vmlalane(vc2.val[3], vb1.val[3], va3.val[2], 3);
+
+    vmlalane(vc2.val[3], vb2.val[0], va3.val[3], 0);
+    vmlalane(vc2.val[3], vb2.val[1], va3.val[3], 1);
+    vmlalane(vc2.val[3], vb2.val[2], va3.val[3], 2);
+    vmlalane(vc2.val[3], vb2.val[3], va3.val[3], 3);
+
+#endif
+
+#if DEBUG
     print_vector(vc1);
     print_vector(vc2);
-#endif 
+#endif
 
-	vstore(&C[0 * N], vc1.val[0]);
-	vstore(&C[1 * N], vc1.val[1]);
-	vstore(&C[2 * N], vc1.val[2]);
-	vstore(&C[3 * N], vc1.val[3]);
+    vstore(&C[0 * N], vc1.val[0]);
+    vstore(&C[1 * N], vc1.val[1]);
+    vstore(&C[2 * N], vc1.val[2]);
+    vstore(&C[3 * N], vc1.val[3]);
 
-	vstore(&C[4 * N], vc2.val[0]);
-	vstore(&C[5 * N], vc2.val[1]);
-	vstore(&C[6 * N], vc2.val[2]);
-	vstore(&C[7 * N], vc2.val[3]);
+    vstore(&C[4 * N], vc2.val[0]);
+    vstore(&C[5 * N], vc2.val[1]);
+    vstore(&C[6 * N], vc2.val[2]);
+    vstore(&C[7 * N], vc2.val[3]);
 }
 
 #elif (N % 4) == 0
 
 void neonMatMul_base(u16 A[], u16 B[], u16 C[])
 {
-	vecval va, vb, vc;
+    vecval va, vb, vc;
 
-	vload(vb.val[0], &B[0 * N]);
-	vload(vb.val[1], &B[1 * N]);
-	vload(vb.val[2], &B[2 * N]);
-	vload(vb.val[3], &B[3 * N]);
+    vload(vb.val[0], &B[0 * N]);
+    vload(vb.val[1], &B[1 * N]);
+    vload(vb.val[2], &B[2 * N]);
+    vload(vb.val[3], &B[3 * N]);
 
-	vload(va.val[0], &A[0 * N]);
-	vload(va.val[1], &A[1 * N]);
-	vload(va.val[2], &A[2 * N]);
-	vload(va.val[3], &A[3 * N]);
+    vload(va.val[0], &A[0 * N]);
+    vload(va.val[1], &A[1 * N]);
+    vload(va.val[2], &A[2 * N]);
+    vload(va.val[3], &A[3 * N]);
 
-	vload(vc.val[0], &C[0 * N]);
-	vload(vc.val[1], &C[1 * N]);
-	vload(vc.val[2], &C[2 * N]);
-	vload(vc.val[3], &C[3 * N]);
+    vload(vc.val[0], &C[0 * N]);
+    vload(vc.val[1], &C[1 * N]);
+    vload(vc.val[2], &C[2 * N]);
+    vload(vc.val[3], &C[3 * N]);
 
-	print_vector(va);
-	print_vector(vb);
-	// printf("===+++=====\n");
-	print_vector(vc);
+    print_vector(va);
+    print_vector(vb);
+    // printf("===+++=====\n");
+    print_vector(vc);
 
-	for (int i = 0; i < BLOCKSIZE; i++)
-	{
-		vmlalane(vc.val[i], vb.val[0], va.val[i], 0);
-		vmlalane(vc.val[i], vb.val[1], va.val[i], 1);
-		vmlalane(vc.val[i], vb.val[2], va.val[i], 2);
-		vmlalane(vc.val[i], vb.val[3], va.val[i], 3);
-	}
-	vstore(&C[0 * N], vc.val[0]);
-	vstore(&C[1 * N], vc.val[1]);
-	vstore(&C[2 * N], vc.val[2]);
-	vstore(&C[3 * N], vc.val[3]);
+    for (int i = 0; i < BLOCKSIZE; i++)
+    {
+        vmlalane(vc.val[i], vb.val[0], va.val[i], 0);
+        vmlalane(vc.val[i], vb.val[1], va.val[i], 1);
+        vmlalane(vc.val[i], vb.val[2], va.val[i], 2);
+        vmlalane(vc.val[i], vb.val[3], va.val[i], 3);
+    }
+    vstore(&C[0 * N], vc.val[0]);
+    vstore(&C[1 * N], vc.val[1]);
+    vstore(&C[2 * N], vc.val[2]);
+    vstore(&C[3 * N], vc.val[3]);
 }
 #else
 #error "Matrix must be multiple of {4, 8}"
