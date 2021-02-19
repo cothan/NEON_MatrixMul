84209fb688bab4ba0a658ae84e7cd80605e71bf8
cothan <cothannguyen@gmail.com>
2021-02-19 04:40:33 -0500

    add 16x16 kernel

--- matmul.c
+++ matmul.c
@@ -8,13 +8,24 @@ typedef uint16_t u16;
 // Correct with N = 4, 12, 20
 // gcc matmul.c -o matmul -O3  -g3 -mcpu=cortex-a9 -mfloat-abi=hard -mfpu=neon
 
-#define N 260
+#define N 16
 #define MAXRAND 8
 #ifndef DEBUG
 #define DEBUG 0
 #endif
 
-#if (N % 8) == 0
+#if (N % 16) == 0
+
+#define BLOCKSIZE 16
+typedef uint16x8_t vec;
+typedef uint16x8x2_t vecval;
+#define vload(c, ptr) c = vld1q_u16_x2(ptr);
+#define vstore(ptr, c) vst1q_u16_x2(ptr, c);
+#define vmla(c, a, b) c = vmlaq_u16(c, a, b);
+#define vmlalane(c, a, b, n) c = vmlaq_laneq_u16(c, a, b, n);
+#define vdup(c, n) c = vdupq_n_u16(n);
+
+#elif (N % 8) == 0
 
 #define BLOCKSIZE 8
 typedef uint16x8_t vec;
@@ -67,6 +78,19 @@ void print_vector(vecval a)
     printf("--\n");
 }
 
+void print_vector_val(vecval a, int val, int bound)
+{
+    for (int i = 0; i < val; i++)
+    {
+        for (int j = 0; j < bound; j++)
+        {
+            printf("%4d, ", a.val[i][j]);
+        }
+        // printf("\n");
+    }
+    printf("--\n");
+}
+
 void classicMatMult(u16 A[], u16 B[], u16 C[])
 {
     for (int i = 0; i < N; i++)
@@ -76,15 +100,57 @@ void classicMatMult(u16 A[], u16 B[], u16 C[])
             for (int j = 0; j < N; j++)
             {
                 C[i * N + j] += A[i * N + k] * B[k * N + j];
-                // printf("%2d * %2d, ", A[i * N + k], B[k * N + j]);
+                printf("%2d * %2d, ", A[i * N + k], B[k * N + j]);
             }
-            // printf("\n");
+            printf("\n");
         }
-        // printf("\n");
+        printf("\n");
+        print_array(C);
     }
 }
 
-#if (N % 8) == 0
+#if (N % 16) == 0
+void neonMatMul_base(u16 A[], u16 B[], u16 C[])
+{
+    // Multiplier block size 16x16
+    // Total registers: 20
+    vecval vb0, vb1, vb2, vb3, vb4, vb5, vb6, vb7, vc, va; //20
+
+    for (int k = 0; k < 16; k++)
+    {
+        vload(va, &A[k*N]);
+        vdup(vc.val[0], 0);
+        vdup(vc.val[1], 0);
+        for (int i = 0; i < 2; i++)
+        {
+            vload(vb0, &B[(8 * i + 0) * N]);
+            vload(vb1, &B[(8 * i + 1) * N]);
+            vload(vb2, &B[(8 * i + 2) * N]);
+            vload(vb3, &B[(8 * i + 3) * N]);
+            vload(vb4, &B[(8 * i + 4) * N]);
+            vload(vb5, &B[(8 * i + 5) * N]);
+            vload(vb6, &B[(8 * i + 6) * N]);
+            vload(vb7, &B[(8 * i + 7) * N]);
+            for (int j = 0; j < 2; j++)
+            {
+                vmlalane(vc.val[j], vb0.val[j], va.val[i], 0);
+                vmlalane(vc.val[j], vb1.val[j], va.val[i], 1);
+                vmlalane(vc.val[j], vb2.val[j], va.val[i], 2);
+                vmlalane(vc.val[j], vb3.val[j], va.val[i], 3);
+                vmlalane(vc.val[j], vb4.val[j], va.val[i], 4);
+                vmlalane(vc.val[j], vb5.val[j], va.val[i], 5);
+                vmlalane(vc.val[j], vb6.val[j], va.val[i], 6);
+                vmlalane(vc.val[j], vb7.val[j], va.val[i], 7);
+            }
+        }
+        vstore(&C[k*N], vc);
+    }
+
+
+
+}
+
+#elif (N % 8) == 0
 void neonMatMul_base(u16 A[], u16 B[], u16 C[])
 {
     // Total registers:  24
@@ -110,15 +176,15 @@ void neonMatMul_base(u16 A[], u16 B[], u16 C[])
     vload(vb2.val[2], &B[6 * N]);
     vload(vb2.val[3], &B[7 * N]);
 
-    vload(vc1.val[0], &C[0 * N]);
-    vload(vc1.val[1], &C[1 * N]);
-    vload(vc1.val[2], &C[2 * N]);
-    vload(vc1.val[3], &C[3 * N]);
+    vdup(vc1.val[0], 0);
+    vdup(vc1.val[1], 0);
+    vdup(vc1.val[2], 0);
+    vdup(vc1.val[3], 0);
 
-    vload(vc2.val[0], &C[4 * N]);
-    vload(vc2.val[1], &C[5 * N]);
-    vload(vc2.val[2], &C[6 * N]);
-    vload(vc2.val[3], &C[7 * N]);
+    vdup(vc2.val[0], 0);
+    vdup(vc2.val[1], 0);
+    vdup(vc2.val[2], 0);
+    vdup(vc2.val[3], 0);
 
     for (int i = 0; i < 4; i++)
     {
@@ -172,10 +238,10 @@ void neonMatMul_base(u16 A[], u16 B[], u16 C[])
     vload(va.val[2], &A[2 * N]);
     vload(va.val[3], &A[3 * N]);
 
-    vload(vc.val[0], &C[0 * N]);
-    vload(vc.val[1], &C[1 * N]);
-    vload(vc.val[2], &C[2 * N]);
-    vload(vc.val[3], &C[3 * N]);
+    vdup(vc.val[0], 0);
+    vdup(vc.val[1], 0);
+    vdup(vc.val[2], 0);
+    vdup(vc.val[3], 0);
 
     for (int i = 0; i < BLOCKSIZE; i++)
     {
@@ -219,7 +285,7 @@ int checkCorrect(u16 A[], u16 B[])
     return 0;
 }
 
-#define TESTS 1000
+#define TESTS 1
 
 int main()
 {
@@ -229,7 +295,7 @@ int main()
     {
         A[i] = rand(); //% MAXRAND;
         B[i] = rand(); //% MAXRAND;
-        // A[i] = (i + 4);
+        // A[i] = i;
         // B[i] = i;
         // C[i] = i ;
         // D[i] = i;
@@ -242,19 +308,19 @@ int main()
     print_array(B);
 #endif
 
-    PAPI_hl_region_begin("classicMatMult");
+    // PAPI_hl_region_begin("classicMatMult");
     for (int i = 0; i < TESTS; i++)
     {
         classicMatMult(A, B, C);
     }
-    PAPI_hl_region_end("classicMatMult");
+    // PAPI_hl_region_end("classicMatMult");
 
-    PAPI_hl_region_begin("neoMatMul");
+    // PAPI_hl_region_begin("neoMatMul");
     for (int i = 0; i < TESTS; i++)
     {
         neoMatMul(A, B, D);
     }
-    PAPI_hl_region_end("neoMatMul");
+    // PAPI_hl_region_end("neoMatMul");
 
 #if DEBUG
     printf("C======\n");
